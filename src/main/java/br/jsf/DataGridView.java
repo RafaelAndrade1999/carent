2c0088c44b08876d928ec75b1d2450831262ea1b
André Rodrigues da Silva <andre.002016@gmail.com>
2018-12-01 10:21:57 -0200

    Listagem busca

--- src/main/java/br/jsf/DataGridView.java
+++ src/main/java/br/jsf/DataGridView.java
@@ -1,13 +1,18 @@
 package br.jsf;
  
+import br.connection.ConnectionFactory;
 import br.jsf.CarService;
 import br.model.Carro;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
  
 @ManagedBean
 @ViewScoped
@@ -25,10 +30,21 @@ public class DataGridView implements Serializable {
         cars = service.createCars(48);
     }
  
-    public List<Carro> getCars() {
-        return cars;
+     public List<Carro> getAll() {
+        EntityManagerFactory factory = ConnectionFactory.getEntityManagerFactory();
+        EntityManager em = factory.createEntityManager();
+        try {
+            List<br.model.Carro> lst = em.createNamedQuery("Carro.findAll").getResultList();
+            return lst;
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+
+            return null;
+        } finally {
+            em.close();
+        }
+
     }
- 
     public void setService(CarService service) {
         this.service = service;
     }
